447e4fc7a2e2626729d711f57dcc4f93df58697b
chung3011 <chung.hitc97@gmail.com>
2021-12-23 11:06:17 +0700

    add method updateData, remove watch

--- index.js
+++ index.js
@@ -14,14 +14,19 @@ new Vue({
     this.tasks = JSON.parse(window.localStorage.getItem(STORAGE) || '[]');
   },
   methods: {
+    updateData(data) {
+      localStorage.setItem(STORAGE, JSON.stringify(data));
+    },
     addTask() {
       this.tasks.push({
         content: this.newTask,
       });
       this.newTask = '';
+      this.updateData(this.tasks);
     },
     deleteTask(task) {
       this.tasks.splice(this.tasks.indexOf(task), 1);
+      this.updateData(this.tasks);
     },
     editTask(task) {
       this.editingTask = task;
@@ -31,14 +36,7 @@ new Vue({
       if (!this.editingTask) return;
       this.editingTask = null;
       task.content = this.editingContent.trim();
-    },
-  },
-  watch: {
-    tasks: {
-      handler() {
-        localStorage.setItem(STORAGE, JSON.stringify(this.tasks));
-      },
-      deep: true,
+      this.updateData(this.tasks);
     },
   },
 });
